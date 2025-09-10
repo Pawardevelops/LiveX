bb26c67d2ab91fd0ff466bd6f61d8a8aba8c98bb
Bhuwan <bhuwanchandra.j@drivex.in>
2025-09-10 15:24:09 +0530

    Added production source maps

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,3 @@
-const isProd = process.env.VERCEL_ENV === "production";
-
 module.exports = {
-  productionBrowserSourceMaps: !isProd,
-  webpack(config, { dev, isServer }) {
-    if (process.env.VERCEL_ENV === "preview") {
-      // Force full source maps with TypeScript inlined
-      config.devtool = "eval-source-map"; // or "inline-source-map"
-    }
-    return config;
-  },
+  productionBrowserSourceMaps: true,
 };
