1706628f7bbee70bb201045ce53a6a60efefcaee
Bhuwan <bhuwanchandra.j@drivex.in>
2025-09-10 15:02:39 +0530

    source map added for production environment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+const isProd = process.env.VERCEL_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  productionBrowserSourceMaps: !isProd,
 };
 
 export default nextConfig;
